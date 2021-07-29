041e6459e0a8daec0396994093868de0ad0642c1
daniel <ddeniel1@yahoo.com.br>
2021-07-29 15:04:17 -0300

    Adicionando Writer Temporário

---
+++ src/main/java/job/writer/LeastSquaresWriterTemp.java
@@ -0,0 +1,10 @@
+package job.writer;
+
+import job.processor.LeastSquares;
+
+public class LeastSquaresWriterTemp implements Writer<LeastSquares>{
+    @Override
+    public void write(LeastSquares input) throws Exception {
+        System.out.println(input);
+    }
+}
